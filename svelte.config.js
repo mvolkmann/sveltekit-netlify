9135a7a58d86427f8559ad6151aa07f0a8a87ca2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-09-12 20:16:56 -0500

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+const netlify = require('@sveltejs/adapter-netlify');
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: netlify(),
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
